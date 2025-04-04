3658fa2fd61b71006720b07c4b3d6d2d113f3a70
Fro Warner <hellofroenn@gmail.com>
2025-04-03 23:27:35 -0700

    Implement brute force solution for longest common prefix

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -2,6 +2,25 @@ package dev.hellofro
 
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
-        return ""
+        if(strs.size == 1){ return strs[0] }
+
+        // Get the shortest string and use it as the prefix
+        val shortestStrAsPrefix = strs.minByOrNull { str -> str.length } ?: return ""
+
+        // Ensure the first char in prefix matches every string
+        if(!strs.all { str -> str.startsWith(shortestStrAsPrefix.substring(0, 1)) }){
+            return ""
+        }
+
+        // expand the prefix using the chars of the shortest string
+        // and check each against the corresponding indices of each string
+        for(i in 1 until shortestStrAsPrefix.length){
+            if(!strs.all { str -> str.startsWith(shortestStrAsPrefix.substring(0, i)) }){
+               return shortestStrAsPrefix.substring(0, i - 1)
+            }
+        }
+
+        // the longest common prefix cannot be longer than the shortest string so we return the original
+        return shortestStrAsPrefix
     }
 }
