a9a07609f5ecad4097c401a102312cd9090b22ed
Fro Warner <hellofroenn@gmail.com>
2025-04-03 23:28:46 -0700

    Add additional basic tests and simplify logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 package dev.hellofro
 
-import dev.hellofro.Solution
-
 fun main() {
     val test1 = arrayOf("flower", "flow", "flight")
     val test2 = arrayOf("dog", "racecar", "car")
+    val test3 = arrayOf("a", "b")
+    val tests = arrayOf(test1, test2, test3)
     val solution = Solution()
 
-    println(solution.longestCommonPrefix(test1))
-    println(solution.longestCommonPrefix(test2))
+    for(test in tests){
+        println(solution.longestCommonPrefix(test))
+    }
 }
